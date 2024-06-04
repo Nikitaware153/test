d96dc2510a57872778cff05d3400f5e6464d62ae
Nikita Taware <nikitaware37@gmail.com>
2024-06-04 20:35:01 +0530

    files added

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request, jsonify
+import pandas as pd
+import numpy as np
+import pickle
+
+
+
+with open('lr_model.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+ 
+    hours_studied = float(request.form['hours_studied'])
+    previous_scores = float(request.form['previous_scores'])
+    extracurricular_activities = float(request.form['extracurricular_activities'])
+    sleep_hours = float(request.form['sleep_hours'])
+    sample_question_papers_practiced = float(request.form['sample_question_papers_practiced'])
+
+    
+    prediction = model.predict([[hours_studied, previous_scores, extracurricular_activities, sleep_hours, sample_question_papers_practiced]])
+
+    
+    return jsonify({'prediction': prediction[0]})
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8080)
+
+
+
+
+
+
